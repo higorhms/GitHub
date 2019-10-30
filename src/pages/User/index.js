c49d2427d129e0fed2d4725dbf099677ad515e3b
higorhms <higorhmscebrac@hotmail.com>
2019-10-29 21:04:33 -0300

    Adicionando LoadMore para recarregar as outras paginas da requisição

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
@@ -28,19 +28,49 @@ export default class User extends Component {
         }).isRequired,
     };
 
-    state = { stars: [] };
+    state = { stars: [], loading: false, page: 1, refreshing: false };
 
     async componentDidMount() {
+        this.setState({ loading: true });
+        this.load();
+    }
+
+    load = async (page = 1) => {
+        const { stars } = this.state;
+        const { navigation } = this.props;
+        const user = navigation.getParam('user');
+        const response = await api.get(`/users/${user.login}/starred`, {
+            params: { page },
+        });
+
+        this.setState({
+            stars: page >= 2 ? [...stars, ...response.data] : response.data,
+            page,
+            loading: false,
+            refreshing: false,
+        });
+    };
+
+    loadMore = async () => {
+        const { page } = this.state;
+
+        const nextPage = page + 1;
+
+        this.load(nextPage);
+    };
+
+    refreshList = async () => {
+        this.setState({ refreshing: true });
         const { navigation } = this.props;
         const user = navigation.getParam('user');
         const response = await api.get(`/users/${user.login}/starred`);
 
-        this.setState({ stars: response.data });
-    }
+        this.setState({ stars: response.data, refreshing: false, page: 1 });
+    };
 
     render() {
         const { navigation } = this.props;
-        const { stars } = this.state;
+        const { stars, loading } = this.state;
         const user = navigation.getParam('user');
         return (
             <Container>
@@ -50,21 +80,29 @@ export default class User extends Component {
                     <Bio>{user.bio}</Bio>
                 </Header>
 
-                <StarsList
-                    data={stars}
-                    keyExtractor={star => String(star.id)}
-                    renderItem={({ item }) => (
-                        <Starred>
-                            <OwnerAvatar
-                                source={{ uri: item.owner.avatar_url }}
-                            />
-                            <Info>
-                                <Title>{item.name}</Title>
-                                <Autor>{item.owner.login}</Autor>
-                            </Info>
-                        </Starred>
-                    )}
-                />
+                {loading ? (
+                    <ActivityIndicator size={50} />
+                ) : (
+                    <StarsList
+                        data={stars}
+                        keyExtractor={star => String(star.id)}
+                        renderItem={({ item }) => (
+                            <Starred>
+                                <OwnerAvatar
+                                    source={{ uri: item.owner.avatar_url }}
+                                />
+                                <Info>
+                                    <Title>{item.name}</Title>
+                                    <Autor>{item.owner.login}</Autor>
+                                </Info>
+                            </Starred>
+                        )}
+                        onEndReachedThreshold={0.2} // Carrega mais itens quando chegar em 20% do fim
+                        onEndReached={this.loadMore} // Função que carrega mais itens
+                        onRefresh={this.refreshList} // Função dispara quando o usuário arrasta a lista pra baixo
+                        refreshing={this.state.refreshing} // Variável que armazena um estado true/false que representa se a lista está atualizando
+                    />
+                )}
             </Container>
         );
     }
