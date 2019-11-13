ef536767e769307419d01779b4ad15870735f837
higorhms <higorhmscebrac@hotmail.com>
2019-11-13 19:05:58 -0300

    Migrando para React Hooks

--- FinderWEB/src/components/Container/index.js
+++ FinderWEB/src/components/Container/index.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+const Container = styled.div`
    max-width: 700px;
    background: #fff;
    border-radius: 4px;
--- FinderWEB/src/pages/Main/index.js
+++ FinderWEB/src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaGithubAlt, FaSpinner, FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
@@ -7,49 +7,43 @@ import api from '../../services/api';
 import Container from '../../components/Container';
 import { Form, SubmitButton, List, Input, ClearButton } from './styles';
 
-export default class Main extends Component {
-   state = {
-      newRepo: '',
-      repositories: [],
-      loading: false,
-      finded: false,
-   };
+export default function Main() {
+   const [newRepo, setNewRepo] = useState('');
+   const [repositories, setRepositories] = useState([]);
+   const [loading, setLoading] = useState(false);
+   const [finded, setFinded] = useState(false);
 
-   componentDidMount() {
-      const repositories = localStorage.getItem('repositories');
+   useEffect(() => {
+      const repos = localStorage.getItem('repositories');
 
-      if (repositories) {
-         this.setState({ repositories: JSON.parse(repositories) });
+      if (repos) {
+         setRepositories(JSON.parse(repos));
+         // setState({ repositories: JSON.parse(repositories) });
       }
+   }, []);
+
+   useEffect(() => {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+   }, [repositories]);
+
+   function handleInputChange(e) {
+      setNewRepo(e.target.value);
    }
 
-   componentDidUpdate(_, prevState) {
-      const { repositories } = this.state;
-
-      if (prevState.repositories !== repositories) {
-         localStorage.setItem('repositories', JSON.stringify(repositories));
-      }
-   }
-
-   handleInputChange = e => {
-      this.setState({ newRepo: e.target.value });
-   };
-
-   clearRepositories = () => {
+   function clearRepositories() {
       localStorage.removeItem('repositories');
-      this.setState({ repositories: [] });
-   };
+      setRepositories([]);
+   }
 
-   handleSubmit = async e => {
+   async function handleSubmit(e) {
       e.preventDefault();
-      this.setState({ loading: true });
-
-      const { newRepo, repositories } = this.state;
+      setLoading(true);
 
       try {
          await api.get(`/repos/${newRepo}`).then(resp => {
             if (repositories.find(repo => repo.id === resp.data.id)) {
-               this.setState({ newRepo: '', loading: false });
+               setNewRepo('');
+               setLoading(true);
                throw new Error('Repositório duplicado');
             }
 
@@ -58,65 +52,61 @@ export default class Main extends Component {
                name: resp.data.full_name,
             };
 
-            this.setState({
-               repositories: [...repositories, data],
-               newRepo: '',
-               loading: false,
-            });
+            setRepositories([...repositories, data]);
+            setNewRepo('');
+            setLoading(false);
          });
       } catch (error) {
-         this.setState({ finded: true, loading: false, newRepo: '' });
+         setFinded(true);
+         setLoading(false);
+         setNewRepo('');
       }
-   };
-
-   render() {
-      const { newRepo, loading, repositories, finded } = this.state;
-
-      return (
-         <Container>
-            <h1>
-               <FaGithubAlt />
-               Repositories
-            </h1>
-            <small>You can find any repository do you want</small>
-            <Form onSubmit={this.handleSubmit}>
-               <Input
-                  finded={finded}
-                  type="text"
-                  placeholder="Example: Owner/repository"
-                  value={newRepo}
-                  onChange={this.handleInputChange}
-               />
-               <SubmitButton loading={loading ? 1 : 0}>
-                  {loading ? (
-                     <p>
-                        <FaSpinner color="#FFF" size={14} /> Loading
-                     </p>
-                  ) : (
-                     <p>
-                        <FaSearch color="#FFF" size={14} /> Find
-                     </p>
-                  )}
-               </SubmitButton>
-            </Form>
-
-            <List>
-               {repositories.map(repo => (
-                  <li key={repo.id}>
-                     <span>{repo.name}</span>
-
-                     <Link to={`/repository/${encodeURIComponent(repo.name)}`}>
-                        Details
-                     </Link>
-                  </li>
-               ))}
-               <div>
-                  <ClearButton onClick={this.clearRepositories}>
-                     Clear Repositories
-                  </ClearButton>
-               </div>
-            </List>
-         </Container>
-      );
    }
+
+   return (
+      <Container>
+         <h1>
+            <FaGithubAlt />
+            Repositories
+         </h1>
+         <small>You can find any repository do you want</small>
+         <Form onSubmit={handleSubmit}>
+            <Input
+               finded={finded}
+               type="text"
+               placeholder="Example: Owner/repository"
+               value={newRepo}
+               onChange={handleInputChange}
+            />
+            <SubmitButton loading={loading ? 1 : 0}>
+               {loading ? (
+                  <p>
+                     <FaSpinner color="#FFF" size={14} /> Loading
+                  </p>
+               ) : (
+                  <p>
+                     <FaSearch color="#FFF" size={14} /> Find
+                  </p>
+               )}
+            </SubmitButton>
+         </Form>
+
+         <List>
+            {repositories.map(repo => (
+               <li key={repo.id}>
+                  <span>{repo.name}</span>
+
+                  <Link to={`/repository/${encodeURIComponent(repo.name)}`}>
+                     Details
+                  </Link>
+               </li>
+            ))}
+            <div>
+               <ClearButton onClick={clearRepositories}>
+                  Clear Repositories
+               </ClearButton>
+            </div>
+         </List>
+      </Container>
+   );
 }
