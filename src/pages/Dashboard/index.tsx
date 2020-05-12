babbf1be9123420b6d43312caab10f33fb83979c
higorhms <higorhmscebrac@hotmail.com>
2020-05-11 23:13:11 -0300

    Refactoring Dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,88 @@
+/* eslint-disable react/jsx-no-target-blank */
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { FaBook } from 'react-icons/fa';
+import api from '../../services/api';
+
+import {
+  Container,
+  RepositoriesContainer,
+  List,
+  ListItem,
+  Name,
+  Description,
+  Language,
+  FollowersContainer,
+  Separator,
+} from './styles';
+import CardFollow from '../../components/CardFollow';
+
+interface Repository {
+  id: number;
+  name: string;
+  description: string;
+  language: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [following, setFollowing] = useState([]);
+  const [followers, setFollowers] = useState([]);
+  const profile = useSelector((state: any) => state.user.profile?.user);
+
+  useEffect(() => {
+    async function fetchApi() {
+      const [
+        repositoriesResponse,
+        followersResponse,
+        followingResponse,
+      ] = await Promise.all([
+        api.get(`/users/${profile.login}/repos`),
+        api.get(`/users/${profile.login}/followers`),
+        api.get(`/users/${profile.login}/following`),
+      ]);
+      setRepositories(repositoriesResponse.data);
+      setFollowers(followersResponse.data);
+      setFollowing(followingResponse.data);
+    }
+    fetchApi();
+  }, [profile]);
+
+  return (
+    <Container container item xs sm>
+      <RepositoriesContainer item xs={12} sm={9}>
+        <h1>Your Repositories</h1>
+        <List>
+          {repositories &&
+            repositories.map((repository) => (
+              <a
+                target="_blank"
+                href={`https://github.com/${profile.login}/${repository.name}`}
+                key={repository.id}
+              >
+                <ListItem>
+                  <Name>
+                    <div>
+                      <FaBook size={15} />
+                      <p>{repository.name}</p>
+                    </div>
+                    <Language>{repository.language}</Language>
+                  </Name>
+                  <Description>{repository.description}</Description>
+                </ListItem>
+              </a>
+            ))}
+        </List>
+      </RepositoriesContainer>
+
+      <FollowersContainer item xs>
+        <h1>Friends</h1>
+        <CardFollow title="Following" list={following} />
+        <Separator />
+        <CardFollow title="Followers" list={followers} />
+      </FollowersContainer>
+    </Container>
+  );
+};
+
+export default Dashboard;
