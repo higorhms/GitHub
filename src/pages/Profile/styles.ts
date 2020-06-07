f03c71f77cd557e4267c92b0db9b3f00a2dc764d
higorhms <higorhmscebrac@hotmail.com>
2020-06-07 12:56:44 -0300

    Responsivity of Profile Component

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -2,11 +2,15 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-
   padding: 30px;
-
   max-width: 1366px;
   margin: 0 auto;
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+  }
 `;
 
 export const Avatar = styled.div`
@@ -14,29 +18,56 @@ export const Avatar = styled.div`
   flex-direction: column;
   max-width: 200px;
 
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+  }
+
   img {
     width: 200px;
     height: 200px;
     border-radius: 20px;
 
     border: 4px solid ${(props) => props.theme.colors.border};
+
+    @media (max-width: 768px) {
+      width: 50px;
+      height: 50px;
+      border-radius: 25px;
+      margin-right: 10px;
+    }
   }
 
   h3 {
     margin-top: 0.2rem;
     font-size: 1.8rem;
     color: ${(props) => props.theme.colors.text};
+
+    @media (max-width: 768px) {
+      font-size: 1.2rem;
+    }
   }
 
   p {
     font-size: 1.4rem;
     color: ${(props) => props.theme.colors.subText};
+
+    @media (max-width: 768px) {
+      display: none;
+    }
   }
 
   span {
     margin-top: 0.5rem;
     font-size: 1rem;
     color: ${(props) => props.theme.colors.text};
+
+    @media (max-width: 768px) {
+      display: none;
+    }
   }
 `;
 
@@ -44,17 +75,24 @@ export const Content = styled.div`
   flex: 1;
   margin-left: 2rem;
 
+  @media (max-width: 768px) {
+    max-width: 100%;
+    margin-left: 0rem;
+  }
+
   ul {
     display: flex;
 
     li {
-      font-size: 1.4rem;
-      font-weight: bold;
       color: ${(props) => props.theme.colors.text};
       transition: 0.3s;
 
       & + li {
         margin-left: 2rem;
+
+        @media (max-width: 768px) {
+          margin-left: 1rem;
+        }
       }
 
       span {
@@ -69,14 +107,32 @@ export const Content = styled.div`
           color: ${(props) => props.theme.colors.border};
         }
 
+        svg {
+          @media (max-width: 768px) {
+            height: 30px;
+            width: 30px;
+          }
+        }
+
         strong {
           display: block;
-          font-size: 20px;
           color: ${(props) => props.theme.colors.text};
           transition: 0.2s;
+          font-weight: bold;
+          font-size: 1.5rem;
+
+          @media (max-width: 768px) {
+            font-size: 1rem;
+          }
 
           :hover {
             color: ${(props) => props.theme.colors.border};
+          }
+        }
+
+        span {
+          @media (max-width: 768px) {
+            font-size: 0.8;
           }
         }
       }
